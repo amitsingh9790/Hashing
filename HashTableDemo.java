3752f9e0a7134d4ae5cb4f2690645c1253a7acc3
Amit Singh <94777067+amitsingh9790@users.noreply.github.com>
2024-08-20 20:04:31 +0530

    Add files via upload

---
+++ HashTableDemo.java
@@ -0,0 +1,16 @@
+package Hashing;
+
+import java.util.Hashtable;
+
+public class HashTableDemo {
+    public static void main(String[] args) {
+        Hashtable hm = new Hashtable();
+        hm.put(102,"Amit");
+        hm.put(109,"Alok");
+        hm.put(113,"Vinay");
+        hm.put(112,"Vijay");
+        hm.put(106,"Arun");
+        hm.put(117,"Kartik");
+        System.out.println(hm);
+    }
+}
